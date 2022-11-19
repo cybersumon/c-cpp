99e915b043b3969915f94b7d7e70b5d99feb8c7e
gmsumon <jsrsumon@gmail.com>
2022-11-19 13:48:39 +0600

    two number multiply with input

---
+++ two-number-multiply.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int c;
+    int a;
+    printf("Enter A Number :");
+    scanf("%d",&a);
+    int b;
+    printf("Enter Second number :");
+    scanf("%d",&b);
+    printf("%d X %d = %d\n",a,b,a*b);
+    getchar;
+    
+}
