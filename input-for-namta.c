bb40e4f2b6a9e4301c8396c2e6a3b55ba88d46ab
gmsumon <jsrsumon@gmail.com>
2022-11-19 16:25:15 +0600

    commit for x and y value

--- input-for-namta.c
+++ input-for-namta.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
-int main()
+int mian()
 {
-    int i,n,r;
-    i = 1;
-    printf("Enter A Number :");
-    scanf("%d",&n);
-    while(i <= 10){
-        printf("%d X %d = %d\n",n,i,n*i);
-        i=i+1;
-    }
+    double x,y,x_plus_y,x_minus_y;
+
+    printf("Enter The Value of x = y :");
+    scanf("%lf",&x_plus_y);
+
+    printf("Enter The Value of X - Y :");
+    scanf("%lf",&x_minus_y);
+
+    x = (x_plus_y + x_minus_y)/2;
+    y = (x_plus_y - x_minus_y)/2;
+
+    printf("x = %0.2lf, y = %0.2lf\n",x,y);
 
     return 0;
-
-
 }
