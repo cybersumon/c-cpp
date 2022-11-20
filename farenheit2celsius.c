92545d53b399806ab9d0d57505a17df2bc196481
gmsumon <jsrsumon@gmail.com>
2022-11-20 19:34:22 +0600

    farenheit2celsius program update

--- farenheit2celsius.c
+++ farenheit2celsius.c
@@ -4,8 +4,8 @@ int main()
     double celsius,farenheit;
     printf("Enter the temperature in farenheit: ");
     scanf("%lf",&farenheit);
-    
-    celsius = 5/9 * farenheit -32;
+
+    celsius =  (farenheit - 32) * 5/9;
     printf("Temperature in celsius is: %0.2lf\n",celsius);
 
     return 0;
