fc49901659066adf4915897ed66a7c27b764896b
gmsumon <jsrsumon@gmail.com>
2022-11-22 21:05:18 +0600

    array program upload

---
+++ array2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a[5] = {10,20,30,40,50};
+
+    printf("First array value is :%d\n",a[0]);
+    printf("Third array value is :%d\n",a[3]);
+    
+    return 0;
+}
---
+++ array3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a[5] = {6,7,8,9,};
+
+    printf("%d %d %d", a[-1],a[7],a[100]);
+
+    return 0;
+}
